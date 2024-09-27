df9a4642956dbc34fabd17a6376bc6a117aaa0c6
Benji8bit <coin8bit@hotmail.com>
2024-09-27 23:26:54 +0300

    Add to TypesValuesVariables object

--- src/lectures/week1base/TypesValuesVariables.scala
+++ src/lectures/week1base/TypesValuesVariables.scala
@@ -1,9 +1,12 @@
 package lectures.week1base
 
 object TypesValuesVariables extends App{
-  private val aString: String = "Hello"
-  println(aString)
-
+  private var aString: String = "Hello"
+  println(aString)  
+  
   val aLong = 11L
   println(aLong)
+  
+  aString = "Hello, world"
+  println(aString)
 }
