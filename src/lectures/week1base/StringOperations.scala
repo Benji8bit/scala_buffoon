9b8136ee02850191ffcd0ae2ff47205024c6dee9
Benji8bit <coin8bit@hotmail.com>
2024-10-01 23:47:05 +0300

    Add to StringOperations object

--- src/lectures/week1base/StringOperations.scala
+++ src/lectures/week1base/StringOperations.scala
@@ -2,7 +2,7 @@ package lectures.week1base
 
 object StringOperations extends App {
   val aString: String = "Scala course"
-  
+
   println(aString.length)
   println(aString.charAt(11))
   println(aString.substring(11, 12))
@@ -13,23 +13,30 @@ object StringOperations extends App {
   println(aString.toUpperCase())
   println("abcd".reverse)
   println(aString.take(-1))
-  
+
   println(aString.take(5).toUpperCase())
   println(aString.toUpperCase().substring(0, 5))
   println(aString.substring(0, 5).toUpperCase())
   println(aString.replace("a", "").take(3).reverse)
-  
+
   val aNumber = "42".toInt
   println(aNumber)
   println(aNumber.getClass)
-  
+
   println('1' +: "42" :+ '3')
   println('a' +: "bc" :+ 'd')
   println("a" ++ "bc" :++ "d")
-  
+
   val name = "John"
   println(s"Hello, $name")
-  
+
   val surname = "Smith"
   println(s"Hello, ${name + surname}")
+
+  val link = "https://stepik.org"
+  println(s"The link is $link.toUpperCase")
+  println(s"The link is ${link.toUpperCase}")
+  println(raw"The link is \t $link")
+  println(link :++ "/catalog")
+  println(s"The link is \t $link")
 }
