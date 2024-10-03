61597d656c7231de78ebb1b7e8f798f2a7b9b2cb
Benji8bit <coin8bit@hotmail.com>
2024-10-03 23:47:36 +0300

    Add to Expressions object

--- src/lectures/week1base/Expressions.scala
+++ src/lectures/week1base/Expressions.scala
@@ -3,4 +3,11 @@ package lectures.week1base
 object Expressions extends App {
   val aVal = 1 + 2 * 3
   println(1 + 2 * 3)
+  
+  val aCondition = true
+  
+  val ifExpressionValue = if (aCondition) "True Condition" else "False Condition"
+  println(ifExpressionValue)
+  
+  println(if(aCondition) "True Condition" else "False Condition")
 }
