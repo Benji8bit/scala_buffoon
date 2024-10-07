55ea38ccef88fe533e502009539fd5f55c418f4b
Benji8bit <coin8bit@hotmail.com>
2024-10-07 23:24:24 +0300

    Add to Functions object

--- src/lectures/week1base/Functions.scala
+++ src/lectures/week1base/Functions.scala
@@ -4,6 +4,23 @@ object Functions extends App {
   def aPerson(name: String, surname: String): String = {
     s"$name $surname"
   }
-
   println(aPerson("John", "Smith"))
+
+  def aParameterlessFunction(): Unit = println("Function with no parameters")
+
+  aParameterlessFunction()
+  //aParameterlessFunction
+  
+  def callByValue(x: Long): Unit = {
+    println(s"call by value: x1 = $x")
+    println(s"call by value: x2 = $x")
+  }
+  
+  def callByName(x: => Long): Unit = {
+    println(s"call by name: x1 = $x")
+    println(s"call by name: x2 = $x")
+  }
+  
+  callByValue(System.nanoTime())
+  callByName(System.nanoTime())
 }
