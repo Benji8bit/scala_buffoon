409f3abe357d6fce97768731e350d843f43a2cf7
Benji8bit <coin8bit@hotmail.com>
2024-10-08 23:54:23 +0300

    Add to Functions object

--- src/lectures/week1base/Functions.scala
+++ src/lectures/week1base/Functions.scala
@@ -10,17 +10,17 @@ object Functions extends App {
 
   aParameterlessFunction()
   //aParameterlessFunction
-  
+
   def callByValue(x: Long): Unit = {
     println(s"call by value: x1 = $x")
     println(s"call by value: x2 = $x")
   }
-  
+
   def callByName(x: => Long): Unit = {
     println(s"call by name: x1 = $x")
     println(s"call by name: x2 = $x")
   }
-  
+
   callByValue(System.nanoTime())
   callByName(System.nanoTime())
 
@@ -28,4 +28,13 @@ object Functions extends App {
   //def callSomeFunc(x: Int, y: => Int) = println(x)
 
   //callSomeFunc(someFunc(), 1)
+
+  // исправьте код
+  def aCondition(): Boolean = if (1 < 2) true else false
+  def multiply(z: Int): Int = z * 2
+  def someFunc(x: Int, y: => Int): Int = {
+    if (aCondition()) x * 2
+    else multiply(y)
+  }
+  println(someFunc(8, 9))
 }
